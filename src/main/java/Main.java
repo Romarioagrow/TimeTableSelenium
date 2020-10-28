be055717cd57004829e84c5744017383ea056d3d
Romarioagrow <Romarioagrow@mail.ru>
2020-10-28 18:55:48 +0500

    scheduleList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Log
 public class Main {
@@ -39,122 +38,17 @@ public class Main {
         calendarDate.click();
 
         /*Собрать данные со страницы*/
-        /*List<WebElement> classes = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.tagName("dd")));*/
-
-        /*List<WebElement> time = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("td.shedule-weekday-time:nth-child(2)")));
-
-        List<WebElement> cabinets = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("tr.shedule-weekday-row:nth-child(2) > td:nth-child(3) > dl:nth-child(1)")));
-*/
-/*
-
-        List<WebElement> time = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("td.shedule-weekday-time:nth-child(2)")));
-
-        List<WebElement> cabinets = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("tr.shedule-weekday-row:nth-child(2) > td:nth-child(3) > dl:nth-child(1)")));
-*/
-
-        //List<WebElement> time = driver.findElements(By.cssSelector("td.shedule-weekday-time:nth-child(2)"));
-
-
         WebDriverWait wait = new WebDriverWait(driver,20);
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("shedule-weekday-item")));
 
-        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        List<WebElement> time = driver.findElements(By.className("shedule-weekday-item"));
-
-        List<WebElement> cabinets = driver.findElements(By.cssSelector("tr.shedule-weekday-row:nth-child(2) > td:nth-child(3) > dl:nth-child(1)"));
-
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-
-        System.out.println("size: " + time.size());
-        int i=0;
-        for(WebElement element1 :time){
-            System.out.println("Section "+i+":"+element1.getText());
-            i++;
-        }
-
-        /*time.forEach(webElement -> {
-            System.out.println("webElement time: " + webElement.toString());
-            System.out.println(webElement.getText());
-            //String ee = element.getAttribute("class");
-            //System.out.println("ee: "  + ee);
-            //System.out.println("time: " + webElement.getText());
-        });*/
-        cabinets.forEach(webElement -> {
-            System.out.println("webElement cabinets: " + webElement.toString());
-            //System.out.println("cabinets: " + webElement.getText());
-        });
-
-        /*List<WebElement> cabinets = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("cabinet")));
-        List<WebElement> teachers = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("teacher")));*/
-
-        //tr.shedule-weekday-row:nth-child(2) > td:nth-child(3) > dl:nth-child(1)
-        /*
-        classes.forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });
-
-        time.forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });
-
-        cabinets.forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });
-        teachers.forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });
-        */
-
-        //showElementData(classes, time, cabinets, teachers);
-
-        /*showElementData(classes);
-        showElementData(time);
-        showElementData(cabinets);
-        showElementData(teachers);*/
+        List<WebElement> scheduleList = driver.findElements(By.className("shedule-weekday-item"));
+        showElementData(scheduleList);
     }
 
-    private static void showElementData(List<WebElement> classes, List<WebElement> times, List<WebElement> cabinets, List<WebElement> teachers) {
-        //System.out.println("WebElement.toString: " + webElement.toString());
-        //System.out.println("WebElement.getText: " + webElement.getText());
-        //elements.forEach(System.out::println);
-
-        int totalClasses = classes.size();
-
-        for (int count = 0; count < totalClasses; count++) {
-            if (classes.get(count).getText() == null) continue;
-
-            String class1 = classes.get(count).getText();
-            String time = times.get(count).getText();
-            String cabinet = cabinets.get(count).getText();
-            String teacher = teachers.get(count).getText();
-
-
-            String result = String.format("Предмет: %s, Время: %s, Ауд: %s, Преподователь: %s", class1, time, cabinet, teacher);
-
-            System.out.println(result);
+    private static void showElementData(List<WebElement> scheduleList) {
+        System.out.println("size: " + scheduleList.size());
+        for(WebElement element1 : scheduleList){
+            System.out.println(element1.getText());
         }
-
-
-        /*elements.forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });*/
-    }
-
-    private static void showElementData(List<WebElement> elements) {
-        //System.out.println("WebElement.toString: " + webElement.toString());
-        //System.out.println("WebElement.getText: " + webElement.getText());
-        //elements.forEach(System.out::println);
-
-        elements.forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });
     }
 }
